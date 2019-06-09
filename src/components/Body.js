ab8cbf0a3af1e97eb08db50484bc1e8807ed232b
Adityesh <adityeshmishra98@gmail.com>
2019-06-09 22:07:04 +0530

    Added functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,12 +10,23 @@ class Body extends React.Component{
             allMemeImgs : []
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
 
     handleChange(event){
         
         this.setState({
             [event.target.name] : event.target.value
+        })
+
+    }
+
+    handleClick(event){
+        event.preventDefault()
+        const rand = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[rand].url
+        this.setState({
+            randomImg : randMemeImg 
         })
 
     }
@@ -38,11 +49,12 @@ class Body extends React.Component{
               <form>
                   <input type="text" placeholder="Top Text" name="topText" value={this.state.topText} onChange={this.handleChange}></input>
                   <input type="text" placeholder="Bottom Text" name="bottomText" value={this.state.bottomText} onChange={this.handleChange}></input>
+                  <button onClick={this.handleClick}>Generate</button>
               </form>
               <div className="meme">
                   <img src={this.state.randomImg}></img>
-                  <h2 className ="top"></h2>
-                  <h2 className="bottom"></h2>
+                  <h2 className ="top">{this.state.topText}</h2>
+                  <h2 className="bottom">{this.state.bottomText}</h2>
               </div>
             </div>
         )
